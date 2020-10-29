9dc5b71fe2bb36b238f43946d45aca3c50e45ff2
SeanyBear <73362700+SeanyBear@users.noreply.github.com>
2020-10-29 14:27:06 -0500

    Update anagramsRunner.java

--- anagramsRunner.java
+++ anagramsRunner.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.*;
 public class anagramsRunner {
 	
 	public String out;
@@ -10,7 +11,7 @@ public class anagramsRunner {
 	public void run() throws Exception
 	{
 		System.out.println("Anagrams- A Project by Chris and Sean");
-			
+		out="";
 		//Begin file input
 		Scanner file = new Scanner(new File("dictionary.txt"));
 		ArrayList<String> dictionary = new ArrayList<String>();
@@ -25,12 +26,42 @@ public class anagramsRunner {
 		String word = ui.next();
 		System.out.println("What is the maximum words to include?");
 		int max = ui.nextInt();
-		recur(word, max, dictionary);
+		ArrayList<String> letters = new ArrayList<String>();
+		for(int i=0;i<word.length();i++)
+			if(!word.substring(i).equals(" "))
+				letters.add(word.substring(i));
+		recur(letters, dictionary);
 	}
 	
-	public void recur(String word, int max, ArrayList<String> dic)
+	public void recur(ArrayList<String> l, ArrayList<String> dic)//add max
 	{
-		
+		ArrayList<String> let=l;
+		boolean check=true;
+		if(l.size()!=0&&dic.size()!=0)
+		{
+			ListIterator<String> i=dic.listIterator();
+			while(i.hasNext())
+			{
+				String word=i.next();
+				for(int j=0;j<word.length();j++)
+				{
+					if(l.indexOf(word.substring(j))==-1)
+					{
+						check=false;
+						i.remove();
+					}
+				}
+				if(check)
+				{
+					for(int j=0;j<word.length();j++)
+					{
+						let.remove(let.indexOf(word.substring(j)));
+					}
+					out=out+word;
+					recur(let, dic);
+				}
+			}
+		}
 	}
 	
 	//Start game
